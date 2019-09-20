2b4bfaf6ad7d110d1328d910eeeef89d1cfdfa2b
borsh357 <alexey.cher357@gmail.com>
2019-09-20 16:31:53 +0300

    finished comments block

--- controls.js
+++ controls.js
@@ -9,13 +9,23 @@ var videoBlock1 = document.getElementById("video-1");
     videoLink3 = document.getElementById('video-link-3');
     videoLink4 = document.getElementById('video-link-4');
 
+    commentsBox = document.getElementById('comment-write-box'); 
+    comments1 = document.getElementById('video-1-comments-block');
+    comments2 = document.getElementById('video-2-comments-block');
+    comments3 = document.getElementById('video-3-comments-block');
+    comments4 = document.getElementById('video-4-comments-block');
+
     videoLink1.onclick = function(e) {
       if (videoBlock1.style.display !== "none") {
         videoBlock1.style.display = "none";
+        comments1.style.display = "none";
         nothingToPlayBlock.style.display = "block";
         videoLink1.style.background = "transparent";
+        commentsBox.style.display = "none";
       } else {
         videoBlock1.style.display = "block";
+        comments1.style.display = "block";
+        commentsBox.style.display = "block";
         videoLink1.style.background = "rgba(159, 206, 245, 0.5)";
         videoBlock2.style.display = "none";
         videoBlock3.style.display = "none";
@@ -24,16 +34,23 @@ var videoBlock1 = document.getElementById("video-1");
         videoLink2.style.background = "transparent";
         videoLink3.style.background = "transparent";
         videoLink4.style.background = "transparent";
+        comments2.style.display = "none";
+        comments3.style.display = "none";
+        comments4.style.display = "none";
       }
     }
 
     videoLink2.onclick = function(e) {
       if (videoBlock2.style.display !== "none") {
         videoBlock2.style.display = "none";
+        comments2.style.display = "none";
         nothingToPlayBlock.style.display = "block";
         videoLink2.style.background = "transparent";
+        commentsBox.style.display = "none";
       } else {
         videoBlock2.style.display = "block";
+        comments2.style.display = "block";
+        commentsBox.style.display = "block";
         videoLink2.style.background = "rgba(159, 206, 245, 0.5)";
         videoBlock1.style.display = "none";
         videoBlock3.style.display = "none";
@@ -42,6 +59,9 @@ var videoBlock1 = document.getElementById("video-1");
         videoLink1.style.background = "transparent";
         videoLink3.style.background = "transparent";
         videoLink4.style.background = "transparent";
+        comments1.style.display = "none";
+        comments3.style.display = "none";
+        comments4.style.display = "none";
       }
     }
 
@@ -50,8 +70,12 @@ var videoBlock1 = document.getElementById("video-1");
         videoBlock3.style.display = "none";
         nothingToPlayBlock.style.display = "block";
         videoLink3.style.background = "transparent";
+        commentsBox.style.display = "none";
+        comments3.style.display = "none";
       } else {
         videoBlock3.style.display = "block";
+        commentsBox.style.display = "block";
+        comments3.style.display = "block";
         videoLink3.style.background = "rgba(159, 206, 245, 0.5)";
         videoBlock1.style.display = "none";
         videoBlock2.style.display = "none";
@@ -60,6 +84,9 @@ var videoBlock1 = document.getElementById("video-1");
         videoLink1.style.background = "transparent";
         videoLink2.style.background = "transparent";
         videoLink4.style.background = "transparent";
+        comments2.style.display = "none";
+        comments1.style.display = "none";
+        comments4.style.display = "none";
       }
     }
 
@@ -68,8 +95,14 @@ var videoBlock1 = document.getElementById("video-1");
         videoBlock4.style.display = "none";
         nothingToPlayBlock.style.display = "block";
         videoLink4.style.background = "transparent";
+        commentsBox.style.display = "none";
+        comments4.style.display = "none";
+        commentsBox.style.display = "none";
       } else {
         videoBlock4.style.display = "block";
+        commentsBox.style.display = "block";
+        commentsBox.style.display = "block";
+        comments4.style.display = "block";
         videoLink4.style.background = "rgba(159, 206, 245, 0.5)";
         videoBlock1.style.display = "none";
         videoBlock2.style.display = "none";
@@ -78,5 +111,8 @@ var videoBlock1 = document.getElementById("video-1");
         videoLink1.style.background = "transparent";
         videoLink2.style.background = "transparent";
         videoLink3.style.background = "transparent";
+        comments2.style.display = "none";
+        comments3.style.display = "none";
+        comments1.style.display = "none";
       }
     }
